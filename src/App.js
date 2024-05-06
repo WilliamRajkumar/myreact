9552e08d2ddbfa8dbeaba3dd2e9cbad50038c1ce
william rajkumar <williamr@smackcoders.com>
2024-05-06 11:26:41 +0530

    new

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
+import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Helmet } from 'react-helmet';
 import Sidebar from './Sidebar';
 import Navbar from './Navbar';
 import ViewScreen from './ViewScreen';
 import Home from './Home';
 import About from './About';
 import Contact from './Contact';
+
 function App() {
   return (
     <Router>
       <div className="container">
+        <Helmet>
+          <title>lacosite</title>
+          <link rel="icon" href=".png-clipart-l.ico" />
+        </Helmet>
         <Sidebar />
         <div className="main-content">
           <Navbar />
@@ -25,6 +32,5 @@ function App() {
     </Router>
   );
 }
-
 
 export default App;
